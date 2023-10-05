b1b58ab926eb02af7cc147354e137847d0f7d100
Arman920 <matevosyan.arman@mail.ru>
2023-10-05 19:47:39 +0400

    first commit

---
+++ src/components/LostFigures.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Figure } from '../models/figures/Figure';
+
+interface LostFiguresProps {
+	title: string;
+	figures: Figure[];
+}
+
+const LostFigures: FC<LostFiguresProps> = ({ title, figures }) => {
+	return (
+		<div className='lost'>
+			<h3>{title}</h3>
+			{figures.map((figure) => (
+				<div key={figure.id}>
+					{figure.logo && (
+						<img
+							width={25}
+							height={25}
+							src={figure.logo}
+						/>
+					)}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default LostFigures;
